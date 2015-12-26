228461b6000fcf04ffa2980417b0b582d621aa80
Joseph Burnitz <joseph.burnitz@orbitz.com>
2015-12-26 12:00:32 -0600

    Add some error handling and tupled results

--- README.md
+++ README.md
@@ -2,4 +2,6 @@
 
 I recently received a log of every single FOIA request that Chicago Police Department has received. This python code is to parse the dates of the requests and do some quick calculations on the log. Read in the dates, add the number of business days since it was received (use a custom calendar due to Chicago having non-standard holidays for local reasons) and calculate when responses are due. This should give us a first order approximation. Also in the interest of transparency, this is a public git repo so that others can check my work. 
 
-tl;dr how often is CPD out of compliance with FOIA law. 
+tl;dr how often is CPD out of compliance with FOIA law.
+with example dataset returns, including some erroneous entries:
+compliant:  12709  Non Compliant >5 days:  3046 
--- analysis.py
+++ analysis.py
@@ -1,23 +1,53 @@
 import pandas as pd
 import csv
-#import time
-#import pytz
 import datetime
 
 from datetime import datetime
 from pandas.tseries.offsets import *
 
 f = open('data.csv', 'r')
-
-requested = []
-completed = []
-# initialize the array before we use it with zeros 
-duedate= [0]*15755
+#tuple of foianum, requestdate, duedate, executeddate, deltadays, compliant
+output=[]
+compliant_count = 0
+non_compliant_count = 0
 for line in f:
-    Request= line.split(",")
-    requested.append(Request[0])
-    completed.append(Request[2])
+    l = line.split(',')
+    foia_num = l[1]
 
-for i in range(len(requested)):
-    duedate[i]=datetime.strptime(requested[i], '%d-%b-%y') + BDay(5)
-    print duedate[i]
+    # null entries
+    if not l[0]:
+        l[0] = '01-Jan-00'
+    request_date = datetime.strptime(l[0], '%d-%b-%y')
+
+    # Some never get filled
+    if not l[2]:
+        l[2] = '31-Dec-99'
+    execute_date = datetime.strptime(l[2], '%d-%b-%y')
+
+    # 5 business days to complete request
+    due_date = request_date + BDay(5)
+
+    delta_days = execute_date - request_date
+    #Give CPD the benefit of the doubt
+    compliant = True
+    if(delta_days.days > 5):
+        compliant = False
+        non_compliant_count+=1
+    else:
+        compliant_count+=1
+
+    output.append(tuple( (foia_num,
+                        request_date.strftime("%Y-%m-%d"),
+                        due_date.strftime("%Y-%m-%d"),
+                        execute_date.strftime("%Y-%m-%d"),
+                        delta_days.days,
+                        compliant) ))
+
+f.close()
+
+for a,b,c,d,e,f in output:
+    #print a,b,c,d,e,f
+    print a,e,f
+
+print "compliant: ",compliant_count,\
+    " Non Compliant >5 days: ", non_compliant_count
